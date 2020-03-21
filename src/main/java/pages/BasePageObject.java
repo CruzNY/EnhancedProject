46c6decab7ac955e6f41155dbea3df5a3fa71ce6
Alex Cruz <alexcruz182@gmail.com>
2020-03-20 20:04:28 -0400

    added property reader for Object repository

--- src/main/java/pages/BasePageObject.java
+++ src/main/java/pages/BasePageObject.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import util.driver.DriverFactory;
+import util.io.PropertyReader;
 
 
 public class BasePageObject extends DriverFactory{
